ac51a49f20874a8ee9f6fdb8d3459d7e2e463feb
anisafebriana0 <146739713+anisafebriana0@users.noreply.github.com>
2025-02-06 19:27:36 +0700

    Add files via upload

---
+++ data/dataset.py
@@ -0,0 +1,33 @@
+import os
+import torchvision
+from config import ANNOTATION_FILE_NAME
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(
+        self,
+        image_directory_path: str,
+        image_processor,
+        train: bool = True
+    ):
+        annotation_file_path = os.path.join(image_directory_path, ANNOTATION_FILE_NAME)
+        super(CocoDetection, self).__init__(image_directory_path, annotation_file_path)
+        self.image_processor = image_processor
+
+    def __getitem__(self, idx):
+        images, annotations = super(CocoDetection, self).__getitem__(idx)
+        image_id = self.ids[idx]
+        annotations = {'image_id': image_id, 'annotations': annotations}
+        encoding = self.image_processor(images=images, annotations=annotations, return_tensors="pt")
+        pixel_values = encoding["pixel_values"].squeeze()
+        target = encoding["labels"][0]
+        return pixel_values, target
+
+def collate_fn(batch, image_processor):
+    pixel_values = [item[0] for item in batch]
+    encoding = image_processor.pad(pixel_values, return_tensors="pt")
+    labels = [item[1] for item in batch]
+    return {
+        'pixel_values': encoding['pixel_values'],
+        'pixel_mask': encoding['pixel_mask'],
+        'labels': labels
+    } 
